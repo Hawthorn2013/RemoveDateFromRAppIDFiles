2358017c246441a75f58347e1b0c237df91912f5
Hawthorn2013 <mynetuser@outlook.com>
2016-03-23 12:36:42 +0800

    Add Single File Process
    - Del date but hold line.
    - Motification in .tmp file.

---
+++ RemoveDateFromRAppIDFiles.py
@@ -0,0 +1,26 @@
+﻿def del_from_file(path):
+    with open(path, 'r') as f:
+        file_list = f.readlines();
+        try:
+            index = file_list[0].index('/*')
+            if index == 0:
+                for list_index in range(len(file_list)):
+                    try:
+                        index = file_list[list_index].index('*/')
+                        if index == 0:
+                            break
+                    except Exception as e:  #Attention: Should not catch all exception.
+                        pass
+                    if file_list[list_index].find('Project Last Save Date') < 0 and file_list[list_index].find('Created on Date') < 0:
+                        #print(file_list[list_index].strip())
+                        pass
+                    else:
+                        file_list[list_index] = ' *\n'
+        except Exception as e:  #Attention: Should not catch all exception.
+            pass
+    print(file_list)
+    with open(path + '.tmp', 'w') as f:
+        f.writelines(file_list)
+    
+print('Hello, Python!')
+del_from_file('RAppIDSrc\default560B.lcf.tmp')
